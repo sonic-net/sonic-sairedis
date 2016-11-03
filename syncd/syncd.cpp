5ca5bcb494c31bf4cab4dacbee12be65747043c0
Kamil Cudnik <kcudnik@microsoft.com>
2016-11-03 12:06:32 -0700

    Fix syncd logging on main enter

--- syncd/syncd.cpp
+++ syncd/syncd.cpp
@@ -1269,7 +1269,7 @@ bool isVeryFirstRun()
 
 int main(int argc, char **argv)
 {
-    swss::Logger::getInstance().setMinPrio(swss::Logger::SWSS_NOTICE);
+    swss::Logger::getInstance().setMinPrio(swss::Logger::SWSS_DEBUG);
 
     SWSS_LOG_ENTER();
 
