2fafbf7039274b65e98852a67e6a8419ae7f48f7
kcudnik <kcudnik@gmail.com>
2018-12-11 18:47:57 +0100

    Move stop notifications trhead after remove switch

--- syncd/syncd.cpp
+++ syncd/syncd.cpp
@@ -3814,13 +3814,13 @@ int syncd_main(int argc, char **argv)
 
     FlexCounter::removeAllCounters();
 
-    // Stop notification thread before removing switch
-    stopNotificationsProcessingThread();
-
     {
         SWSS_LOG_TIMER("remove switch");
         status = sai_switch_api->remove_switch(gSwitchId);
     }
+
+    // Stop notification thread after removing switch
+    stopNotificationsProcessingThread();
 
     if (status != SAI_STATUS_SUCCESS)
     {
