8308b63307b40621962876502b6d4204a0516a24
kcudnik <kcudnik@gmail.com>
2019-12-02 15:51:58 +0100

    Move port map and port map parser to proper class

---
+++ syncd/PortMapParser.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "syncd.h"
+#include "PortMap.h"
+
+#include <memory>
+
+class PortMapParser
+{
+    private:
+
+        PortMapParser() = delete;
+        ~PortMapParser() = delete;
+
+    public:
+
+        static std::shared_ptr<PortMap> parsePortMap(
+                _In_ const std::string& portMapFile);
+};
