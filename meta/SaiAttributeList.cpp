39d6a1a349aca3e28b433ade02f0b4e2d9c1804d
chiourung_huang <chiourung_huang@edge-core.com>
2020-05-07 02:26:58 +0000

    remove new flag

--- meta/SaiAttributeList.cpp
+++ meta/SaiAttributeList.cpp
@@ -35,9 +35,6 @@ SaiAttributeList::SaiAttributeList(
             SWSS_LOG_THROW("FATAL: failed to find metadata for object type %d and attr id %d", objectType, attr.id);
         }
 
-        if (SAI_HAS_FLAG_IDENTIFY(meta->flags))
-            continue;
-
         sai_deserialize_attr_value(str_attr_value, *meta, attr, countOnly);
 
         m_attr_list.push_back(attr);
@@ -73,9 +70,6 @@ SaiAttributeList::SaiAttributeList(
         {
             SWSS_LOG_THROW("FATAL: failed to find metadata for object type %d and attr id %d", objectType, attr.id);
         }
-
-        if (SAI_HAS_FLAG_IDENTIFY(meta->flags))
-            continue;
 
         sai_deserialize_attr_value(str_attr_value, *meta, attr, countOnly);
 
