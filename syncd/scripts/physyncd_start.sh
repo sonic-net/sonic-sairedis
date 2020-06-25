9e2d0c437ca06240b529c3e9ef287c350c02b081
Syd Logan <slogan621@gmail.com>
2020-06-25 13:08:29 -0700

    revert syncd_init_common.sh to support only launching syncd, modify physyncd_start.sh to launch via python script bypassing syncd_init_common

--- syncd/scripts/physyncd_start.sh
+++ syncd/scripts/physyncd_start.sh
@@ -3,10 +3,5 @@
 # Script to start syncd using supervisord
 #
 
-# Source the file that holds common code for systemd and supervisord
-. /usr/bin/syncd_init_common.sh -p
-
-config_syncd_phy
-
 exec "/usr/bin/physyncd_startup.py"
 
