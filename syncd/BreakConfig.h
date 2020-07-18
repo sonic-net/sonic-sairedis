790575405e7db12d3a3a0f0934d7146d9b1cba21
kcudnik <kcudnik@gmail.com>
2020-07-18 18:52:52 +0200

    [syncd] Add BreakConfig class

---
+++ syncd/BreakConfig.h
@@ -0,0 +1,37 @@
+#pragma once
+
+extern "C"{
+#include "sai.h"
+}
+
+#include <set>
+
+namespace syncd
+{
+    class BreakConfig
+    {
+        public:
+
+            BreakConfig() = default;
+
+            ~BreakConfig() = default;
+
+        public:
+
+            void insert(
+                    _In_ sai_object_type_t objectType);
+
+            void remove(
+                    _In_ sai_object_type_t objectType);
+
+            void clear();
+
+            bool shouldBreakBeforeMake(
+                    _In_ sai_object_type_t objectType) const;
+
+        private:
+
+            std::set<sai_object_type_t> m_set;
+
+    };
+}
