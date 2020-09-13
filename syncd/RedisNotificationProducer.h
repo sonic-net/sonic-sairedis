8b519539d6d8beb7a2baec1877fdba85824f64c7
kcudnik <kcudnik@gmail.com>
2020-09-13 23:32:07 +0200

    [syncd] Add RedisNotificationProducer class

---
+++ syncd/RedisNotificationProducer.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include "RedisNotificationProducer.h"
+#include "NotificationProducerBase.h"
+
+#include "swss/dbconnector.h"
+#include "swss/notificationproducer.h"
+
+namespace syncd
+{
+    class RedisNotificationProducer:
+        public NotificationProducerBase
+    {
+        public:
+
+            RedisNotificationProducer(
+                    _In_ const std::string& dbName);
+
+            virtual ~RedisNotificationProducer() = default;
+
+        public:
+
+            virtual void send(
+                    _In_ const std::string& op, 
+                    _In_ const std::string& data, 
+                    _In_ const std::vector<swss::FieldValueTuple>& values) override;
+
+        private:
+
+            std::shared_ptr<swss::DBConnector> m_db;
+
+            std::shared_ptr<swss::NotificationProducer> m_notificationProducer;
+    };
+}
+
