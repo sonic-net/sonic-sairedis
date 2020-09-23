bb9d8efdefb252851f6c108afd699a53d638e153
Ze Gan <ganze718@gmail.com>
2020-09-23 17:17:01 +0800

    Fix binary 2 hex bug
    Signed-off-by: Ze Gan <ganze718@gmail.com>

--- meta/saiserialize.cpp
+++ meta/saiserialize.cpp
@@ -913,6 +913,14 @@ std::string sai_serialize_queue_attr(
     return sai_serialize_enum(attr, &sai_metadata_enum_sai_queue_attr_t);
 }
 
+std::string sai_serialize_macsec_sa_attr(
+        _In_ const  sai_macsec_sa_attr_t attr)
+{
+    SWSS_LOG_ENTER();
+
+    return sai_serialize_enum(attr, &sai_metadata_enum_sai_macsec_sa_attr_t);
+}
+
 std::string sai_serialize_switch_oper_status(
         _In_ sai_object_id_t switch_id,
         _In_ sai_switch_oper_status_t status)
@@ -1381,7 +1389,7 @@ std::string sai_serialize_hex_binary(const void *buffer, size_t length)
     {
         return s;
     }
-    const char *input = (const char *)buffer;
+    const unsigned char *input = (const unsigned char *)buffer;
     char *output = &s[0];
     for (unsigned long long i = 0; i < length; i++)
     {
@@ -3367,6 +3375,15 @@ void sai_deserialize_queue_attr(
     SWSS_LOG_ENTER();
 
     sai_deserialize_enum(s, &sai_metadata_enum_sai_queue_attr_t, (int32_t&)attr);
+}
+
+void sai_deserialize_macsec_sa_attr(
+        _In_ const std::string& s,
+        _Out_ sai_macsec_sa_attr_t& attr)
+{
+    SWSS_LOG_ENTER();
+
+    sai_deserialize_enum(s, &sai_metadata_enum_sai_macsec_sa_attr_t, (int32_t&)attr);
 }
 
 // sairedis
