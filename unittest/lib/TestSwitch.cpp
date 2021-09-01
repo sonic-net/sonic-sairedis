80f79ec87f55c82a76b7b68fa324819079f76467
kcudnik <kcudnik@gmail.com>
2021-09-01 14:52:53 +0200

    [tests] Remove new operator from create objects

--- unittest/lib/TestSwitch.cpp
+++ unittest/lib/TestSwitch.cpp
@@ -6,5 +6,5 @@ using namespace sairedis;
 
 TEST(Switch, ctr)
 {
-    EXPECT_THROW(new Switch(SAI_NULL_OBJECT_ID), std::runtime_error);
+    EXPECT_THROW(std::make_shared<Switch>(SAI_NULL_OBJECT_ID), std::runtime_error);
 }
