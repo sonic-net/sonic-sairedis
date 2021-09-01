a67abc1232df914130ec689da01e6168c5756cf9
kcudnik <kcudnik@gmail.com>
2021-09-01 15:21:45 +0200

    Fix ClientConfig tests

--- unittest/lib/TestClientConfig.cpp
+++ unittest/lib/TestClientConfig.cpp
@@ -8,7 +8,7 @@ TEST(ClientConfig, loadFromFile)
 {
     EXPECT_NE(ClientConfig::loadFromFile("non_existing"), nullptr);
 
-    EXPECT_NE(ClientConfig::loadFromFile("files/client_config_bar.txt"), nullptr);
+    EXPECT_NE(ClientConfig::loadFromFile("files/client_config_bad.txt"), nullptr);
 
     EXPECT_NE(ClientConfig::loadFromFile("files/client_config_ok.txt"), nullptr);
 }
