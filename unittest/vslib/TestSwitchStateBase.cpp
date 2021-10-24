be153a39e509cf25907ff480d6aee275c704a466
Ze Gan <ganze718@gmail.com>
2021-10-24 02:46:37 +0000

    Add SWSS_LOG_ENTER
    Signed-off-by: Ze Gan <ganze718@gmail.com>

--- unittest/vslib/TestSwitchStateBase.cpp
+++ unittest/vslib/TestSwitchStateBase.cpp
@@ -16,35 +16,39 @@ using namespace saivs;
 
 TEST(SwitchStateBase, loadMACsecAttrFromMACsecSC)
 {
+    SWSS_LOG_ENTER();
+
     auto sc = std::make_shared<SwitchConfig>(0, "");
     auto scc = std::make_shared<SwitchConfigContainer>();
-
     SwitchStateBase ssb(
             0x2100000000,
             std::make_shared<RealObjectIdManager>(0, scc),
             sc);
-
     sai_attribute_t attr;
     MACsecAttr macsecAttr;
 
     attr.id = SAI_MACSEC_SC_ATTR_MACSEC_CIPHER_SUITE;
     attr.value.s32 = sai_macsec_cipher_suite_t::SAI_MACSEC_CIPHER_SUITE_GCM_AES_128;
     ssb.loadMACsecAttrFromMACsecSC(0, 1 , &attr, macsecAttr);
+
     EXPECT_EQ(macsecAttr.m_cipher, CIPHER_NAME_GCM_AES_128);
 
     attr.id = SAI_MACSEC_SC_ATTR_MACSEC_CIPHER_SUITE;
     attr.value.s32 = sai_macsec_cipher_suite_t::SAI_MACSEC_CIPHER_SUITE_GCM_AES_256;
     ssb.loadMACsecAttrFromMACsecSC(0, 1 , &attr, macsecAttr);
+
     EXPECT_EQ(macsecAttr.m_cipher, CIPHER_NAME_GCM_AES_256);
 
     attr.id = SAI_MACSEC_SC_ATTR_MACSEC_CIPHER_SUITE;
     attr.value.s32 = sai_macsec_cipher_suite_t::SAI_MACSEC_CIPHER_SUITE_GCM_AES_XPN_128;
     ssb.loadMACsecAttrFromMACsecSC(0, 1 , &attr, macsecAttr);
+
     EXPECT_EQ(macsecAttr.m_cipher, CIPHER_NAME_GCM_AES_XPN_128);
 
     attr.id = SAI_MACSEC_SC_ATTR_MACSEC_CIPHER_SUITE;
     attr.value.s32 = sai_macsec_cipher_suite_t::SAI_MACSEC_CIPHER_SUITE_GCM_AES_XPN_256;
     ssb.loadMACsecAttrFromMACsecSC(0, 1 , &attr, macsecAttr);
+
     EXPECT_EQ(macsecAttr.m_cipher, CIPHER_NAME_GCM_AES_XPN_256);
 
 }
