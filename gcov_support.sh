d97e588faa74060cb35888ef9e3f91c228079af7
pettershao-ragilenetworks <pettershao@ragilenetworks.com>
2021-11-23 16:46:37 +0800

    update patch
    Signed-off-by: pettershao-ragilenetworks <pettershao@ragilenetworks.com>

--- gcov_support.sh
+++ gcov_support.sh
@@ -367,14 +367,22 @@ gcov_support_collect_gcno()
         # temporarily using fixed dir
         echo " === ls -lh work_dir/debian === "
         ls -lh ${work_dir}/debian/
-        mkdir -p ${work_dir}/debian/$submodule_name/tmp/gcov
-        cp gcno_$submodule_name.tar.gz ${work_dir}/debian/$submodule_name/tmp/gcov
-        cp ./gcov_support.sh ${work_dir}/debian/$submodule_name/tmp/gcov
-        cp ./gcovpreload/lcov_cobertura.py ${work_dir}/debian/$submodule_name/tmp/gcov
-        mkdir -p ${work_dir}/debian/$submodule_name/usr
-        mkdir -p ${work_dir}/debian/$submodule_name/usr/lib
-        cp ./gcovpreload/libgcovpreload.so ${work_dir}/debian/$submodule_name/usr/lib
-        sudo chmod 777 -R /${work_dir}/debian/$submodule_name/usr/lib/libgcovpreload.so
+        # mkdir -p ${work_dir}/debian/$submodule_name/tmp/gcov
+        # cp gcno_$submodule_name.tar.gz ${work_dir}/debian/$submodule_name/tmp/gcov
+        # cp ./gcov_support.sh ${work_dir}/debian/$submodule_name/tmp/gcov
+        # cp ./gcovpreload/lcov_cobertura.py ${work_dir}/debian/$submodule_name/tmp/gcov
+        # mkdir -p ${work_dir}/debian/$submodule_name/usr
+        # mkdir -p ${work_dir}/debian/$submodule_name/usr/lib
+        # cp ./gcovpreload/libgcovpreload.so ${work_dir}/debian/$submodule_name/usr/lib
+        # sudo chmod 777 -R /${work_dir}/debian/$submodule_name/usr/lib/libgcovpreload.so
+        mkdir -p ${work_dir}/debian/tmp/gcov
+        cp gcno_$submodule_name.tar.gz ${work_dir}/debian/tmp/gcov
+        cp ./gcov_support.sh ${work_dir}/debian/tmp/gcov
+        cp ./gcovpreload/lcov_cobertura.py ${work_dir}/debian/tmp/gcov
+        mkdir -p ${work_dir}/debian/tmp/usr
+        mkdir -p ${work_dir}/debian/tmp/usr/lib
+        cp ./gcovpreload/libgcovpreload.so ${work_dir}/debian/tmp/usr/lib
+        sudo chmod 777 -R /${work_dir}/debian/tmp/usr/lib/libgcovpreload.so
         rm $GCNO_LIST_FILE
         echo " === Collect finished... === "
     fi
