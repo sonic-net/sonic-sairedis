f3a6753978d8fd399f426bcf36d491de5590d3a9
songqh2022 <songqh@clounix.com>
2022-03-17 18:45:13 +0800

    Support for platforms based on Clounix Networks' device

--- syncd/scripts/syncd_init_common.sh
+++ syncd/scripts/syncd_init_common.sh
@@ -197,6 +197,10 @@ config_syncd_vs()
     CMD_ARGS+=" -p $HWSKU_DIR/sai.profile"
 }
 
+config_syncd_clounix()
+{
+    CMD_ARGS+=" -p $HWSKU_DIR/sai.profile"
+}
 config_syncd_innovium()
 {
     CMD_ARGS+=" -p $HWSKU_DIR/sai.profile"
@@ -235,6 +239,8 @@ config_syncd()
         config_syncd_nephos
     elif [ "$SONIC_ASIC_TYPE" == "vs" ]; then
         config_syncd_vs
+    elif [ "$SONIC_ASIC_TYPE" == "clounix" ]; then
+        config_syncd_clounix
     elif [ "$SONIC_ASIC_TYPE" == "innovium" ]; then
         config_syncd_innovium
     elif [ "$SONIC_ASIC_TYPE" == "cisco-8000" ]; then
