6e9378b55bb60d9b14707c32a9327cbeffc25217
Junhua Zhai <junhua.zhai@outlook.com>
2022-10-18 09:04:54 +0000

    Add REDIS_TABLE_NOTIFICATIONS_PER_DB

--- syncd/RedisNotificationProducer.cpp
+++ syncd/RedisNotificationProducer.cpp
@@ -13,8 +13,7 @@ RedisNotificationProducer::RedisNotificationProducer(
 
     m_db = std::make_shared<swss::DBConnector>(dbName, 0);
 
-    m_notificationProducer = std::make_shared<swss::NotificationProducer>(m_db.get(),
-        dbName == "GB_ASIC_DB" ? REDIS_TABLE_GBNOTIFICATIONS : REDIS_TABLE_NOTIFICATIONS);
+    m_notificationProducer = std::make_shared<swss::NotificationProducer>(m_db.get(), REDIS_TABLE_NOTIFICATIONS_PER_DB(dbName));
 }
 
 void RedisNotificationProducer::send(
