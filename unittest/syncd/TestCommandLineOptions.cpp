f947f90fff3ab06e4d0bc5f5ca72753dbc52e583
Oleksandr Ivantsiv <oivantsiv@nvidia.com>
2023-05-19 15:26:51 +0300

    Add test for m_watchdogWarnTimeSpan option.

--- unittest/syncd/TestCommandLineOptions.cpp
+++ unittest/syncd/TestCommandLineOptions.cpp
@@ -1,6 +1,8 @@
 #include "CommandLineOptions.h"
+#include "CommandLineOptionsParser.h"
 
 #include <gtest/gtest.h>
+#include <algorithm>
 
 using namespace syncd;
 
@@ -21,4 +23,15 @@ TEST(CommandLineOptions, startTypeStringToStartType)
     auto st = syncd::CommandLineOptions::startTypeStringToStartType("foo");
 
     EXPECT_EQ(st, SAI_START_TYPE_UNKNOWN);
+}
+
+TEST(CommandLineOptionsParser, parseCommandLine)
+{
+    char arg1[] = "test";
+    char arg2[] = "-w";
+    char arg3[] = "1000";
+    std::vector<char *> args = {arg1, arg2, arg3};
+
+    auto opt = syncd::CommandLineOptionsParser::parseCommandLine((int)args.size(), args.data());
+    EXPECT_EQ(opt->m_watchdogWarnTimeSpan, 1000);
 }
