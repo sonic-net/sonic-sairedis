sonic-net/sonic-sairedis

commit 594944dbe4414f135fc6366a30118afa10c135eb

Fix flow counter out-of-order issue by notifying counter operations using SelectableChannel (#1362)
What I did

Fix flow counter out-of-order issue by notifying counter operations using SelectableChannel

Signed-off-by: Stephen Sun stephens@nvidia.com

Why I did it

Currently, the operations of SAI objects and their counters (if any) are triggered by different channels, which introduces racing conditions:

the creation and destruction of the objects are notified using the SelectableChannel,
the operations of counters, including starting and stopping polling the counters, are notified by listening to the FLEX_COUNTER and FLEX_COUNTER_GROUP tables in the FLEX_COUNTER_DB
The orchagent always respects the order when starting/stopping counter-polling (which means to start counter-polling after creating the object and to stop counter-polling before destroying the object) but syncd can receive events in a wrong order, eg. it receives destroying an object first and then stopping counter polling on the object, it can poll counter for a non-exist object, which causes errors in vendor SAI.
The new solution is to extend SAI redis attributes on the SAI_SWITCH_OBJECT to notify counter polling. As a result, all the objects and their counters are notified using a unified channel, which is the SelectableChannel.

How I verified it

Unit test
Manual test
Regressions test

Details if related

There are two SAI Redis attributes introduced as below. There are some fields with const char * type for each attribute. Passing a field as nullptr means not to change it.

SAI_REDIS_SWITCH_ATTR_FLEX_COUNTER_GROUP for counters represented by FLEX_COUNTER_GROUP table in the FLEX_COUNTER_DB, including the following fields
counter_group_name, which is the key of the table, representing the group name.
poll_interval, which is the field POLL_INTERVAL of an entry, representing the polling interval of the group.

lib/RedisRemoteSaiInterface.cpp
@@ -476,6 +476,14 @@ sai_status_t RedisRemoteSaiInterface::setRedisExtensionAttribute(
 
             return SAI_STATUS_SUCCESS;
 
+        case SAI_REDIS_SWITCH_ATTR_FLEX_COUNTER_GROUP:
+            return notifyCounterGroupOperations(objectId,
+                                                reinterpret_cast<sai_redis_flex_counter_group_parameter_t*>(attr->value.ptr));
+
+        case SAI_REDIS_SWITCH_ATTR_FLEX_COUNTER:
+            return notifyCounterOperations(objectId,
+                                           reinterpret_cast<sai_redis_flex_counter_parameter_t*>(attr->value.ptr));
+
         default:
             break;
     }
@@ -483,6 +491,126 @@ sai_status_t RedisRemoteSaiInterface::setRedisExtensionAttribute(
     SWSS_LOG_ERROR("unknown redis extension attribute: %d", attr->id);
 
     return SAI_STATUS_FAILURE;
+}
+
+bool RedisRemoteSaiInterface::isSaiS8ListValidString(
+        _In_ const sai_s8_list_t &s8list)
+{
+    SWSS_LOG_ENTER();
+
+    if (s8list.list != nullptr && s8list.count > 0)
+    {
+        size_t len = strnlen((const char *)s8list.list, s8list.count);
+
+        if (len == (size_t)s8list.count)
+        {
+            return true;
+        }
+        else
+        {
+            SWSS_LOG_ERROR("Count (%u) is different than strnlen (%zu)", s8list.count, len);
+        }
+    }
+
+    return false;
+}
+
+bool RedisRemoteSaiInterface::emplaceStrings(
+        _In_ const sai_s8_list_t &field,
+        _In_ const sai_s8_list_t &value,
+        _Out_ std::vector<swss::FieldValueTuple> &entries)
+{
+    SWSS_LOG_ENTER();
+
+    bool result = false;
+
+    if (isSaiS8ListValidString(field) && isSaiS8ListValidString(value))
+    {
+        entries.emplace_back(std::string((const char*)field.list, field.count), std::string((const char*)value.list, value.count));
+        result = true;
+    }
+
+    return result;
+}
+
+bool RedisRemoteSaiInterface::emplaceStrings(
+        _In_ const char *field,
+        _In_ const sai_s8_list_t &value,
+        _Out_ std::vector<swss::FieldValueTuple> &entries)
+{
+    SWSS_LOG_ENTER();
+
+    bool result = false;
+
+    if (isSaiS8ListValidString(value))
+    {
+        entries.emplace_back(field, std::string((const char*)value.list, value.count));
+        result = true;
+    }
+
+    return result;
+}
+
+sai_status_t RedisRemoteSaiInterface::notifyCounterGroupOperations(
+        _In_ sai_object_id_t objectId,
+        _In_ const sai_redis_flex_counter_group_parameter_t *flexCounterGroupParam)
+{
+    SWSS_LOG_ENTER();
+
+    std::vector<swss::FieldValueTuple> entries;
+
+    if (flexCounterGroupParam == nullptr || !isSaiS8ListValidString(flexCounterGroupParam->counter_group_name))
+    {
+        SWSS_LOG_ERROR("Invalid parameters when handling counter group operation");
+        return SAI_STATUS_FAILURE;
+    }
+
+    std::string key((const char*)flexCounterGroupParam->counter_group_name.list, flexCounterGroupParam->counter_group_name.count);
+
+    emplaceStrings(POLL_INTERVAL_FIELD, flexCounterGroupParam->poll_interval, entries);
+    emplaceStrings(STATS_MODE_FIELD, flexCounterGroupParam->stats_mode, entries);
+    emplaceStrings(flexCounterGroupParam->plugin_name, flexCounterGroupParam->plugins, entries);
+    emplaceStrings(FLEX_COUNTER_STATUS_FIELD, flexCounterGroupParam->operation, entries);
+
+    m_recorder->recordGenericSet(key, entries);
+
+    m_communicationChannel->set(key,
+                                entries,
+                                (entries.size() != 0) ? REDIS_FLEX_COUNTER_COMMAND_SET_GROUP : REDIS_FLEX_COUNTER_COMMAND_DEL_GROUP);
+
+    return waitForResponse(SAI_COMMON_API_SET);
+}
+
+sai_status_t RedisRemoteSaiInterface::notifyCounterOperations(
+        _In_ sai_object_id_t objectId,
+        _In_ const sai_redis_flex_counter_parameter_t *flexCounterParam)
+{
+    SWSS_LOG_ENTER();
+
+    if (flexCounterParam == nullptr || !isSaiS8ListValidString(flexCounterParam->counter_key))
+    {
+        SWSS_LOG_ERROR("Invalid parameters when handling counter operation");
+        return SAI_STATUS_FAILURE;
+    }
+
+    std::vector<swss::FieldValueTuple> entries;
+    std::string key((const char*)flexCounterParam->counter_key.list, flexCounterParam->counter_key.count);
+    std::string command;
+
+    if (emplaceStrings(flexCounterParam->counter_field_name, flexCounterParam->counter_ids, entries))
+    {
+        command = REDIS_FLEX_COUNTER_COMMAND_START_POLL;
+        emplaceStrings(STATS_MODE_FIELD, flexCounterParam->stats_mode, entries);
+    }
+    else
+    {
+        command = REDIS_FLEX_COUNTER_COMMAND_STOP_POLL;
+    }
+
+    m_recorder->recordGenericSet(key, entries);
+    m_communicationChannel->set(key, entries, command);
+
+    return waitForResponse(SAI_COMMON_API_SET);
 }
 
 sai_status_t RedisRemoteSaiInterface::set(

lib/RedisRemoteSaiInterface.h
@@ -343,6 +343,27 @@ namespace sairedis
                     _In_ sai_object_id_t objectId,
                     _In_ const sai_attribute_t *attr);
 
+            bool isSaiS8ListValidString(
+                    _In_ const sai_s8_list_t &s8list);
+
+            bool emplaceStrings(
+                    _In_ const sai_s8_list_t &field,
+                    _In_ const sai_s8_list_t &value,
+                    _Out_ std::vector<swss::FieldValueTuple> &entries);
+
+            bool emplaceStrings(
+                    _In_ const char *field,
+                    _In_ const sai_s8_list_t &value,
+                    _Out_ std::vector<swss::FieldValueTuple> &entries);
+
+            sai_status_t notifyCounterGroupOperations(
+                    _In_ sai_object_id_t objectId,
+                    _In_ const sai_redis_flex_counter_group_parameter_t *flexCounterGroupParam);
+
+            sai_status_t notifyCounterOperations(
+                    _In_ sai_object_id_t objectId,
+                    _In_ const sai_redis_flex_counter_parameter_t *flexCounterParam);
+
         private:
 
             sai_status_t sai_redis_notify_syncd(

lib/sairedis.h
@@ -86,6 +86,88 @@ typedef enum _sai_redis_communication_mode_t
 
 } sai_redis_communication_mode_t;
 
+/**
+ * @brief Use Redis communication channel to handle counters.
+ *
+ * Originally, there are out-of-order issue between the objects and their counters.
+ * This is because the counters are handled on receiving flex counter database update.
+ * However, the objects are handled on receiving update from the redis communication channel.
+ *
+ * To resolve the issue, we use the redis communication channel to handle the counter updates.
+ *
+ * The struct sai_redis_flex_counter_group_parameter_t represents the counter group operations.
+ * The caller (usually orchagent) can change some or all the options of a counter group.
+ * The counter_group_name represents the counter group name which must be a valid list.
+ * For the rest fields, it means not changing it to pass an empty list.
+ */
+typedef struct _sai_redis_flex_counter_group_parameter_t
+{
+    /**
+     * @brief The flex counter group name.
+     *
+     * It is the key of FLEX_COUNTER_TABLE and FLEX_COUNTER_GROUP_TABLE table.
+     */
+    sai_s8_list_t counter_group_name;
+
+    /**
+     * @brief The polling interval of the counter group
+     *
+     * It should be a number representing the polling interval in seconds.
+     */
+    sai_s8_list_t poll_interval;
+
+    /**
+     * @brief The operation of the counter group
+     *
+     * It should be either "enable" or "disable"
+     */
+    sai_s8_list_t operation;
+
+    /**
+     * @brief The counter fetching mode.
+     *
+     * It should be either "STATS_MODE_READ" or "STATS_MODE_READ_AND_CLEAR"
+     */
+    sai_s8_list_t stats_mode;
+
+    /**
+     * @brief The name of the filed that represents the Lua plugin
+     */
+    sai_s8_list_t plugin_name;
+
+    /**
+     * @brief The SHA code of the Lua plugin
+     */
+    sai_s8_list_t plugins;
+
+} sai_redis_flex_counter_group_parameter_t;
+
+typedef struct _sai_redis_flex_counter_parameter_t
+{
+    /**
+     * @brief The key in the flex counter table
+     *
+     * It should be the serialized OID eg. "oid:0x15000000000001"
+     */
+    sai_s8_list_t counter_key;
+
+    /**
+     * @brief The list of counters' IDs that should be fetched.
+     */
+    sai_s8_list_t counter_ids;
+
+    /**
+     * @brief The name of the filed that represents the counters' IDs.
+     */
+    sai_s8_list_t counter_field_name;
+
+    /**
+     * @brief The counter fetch mode of the object.
+     */
+    sai_s8_list_t stats_mode;
+
+} sai_redis_flex_counter_parameter_t;
+
 typedef enum _sai_redis_switch_attr_t
 {
     /**
@@ -249,6 +331,25 @@ typedef enum _sai_redis_switch_attr_t
      * @default 60000
      */
     SAI_REDIS_SWITCH_ATTR_SYNC_OPERATION_RESPONSE_TIMEOUT,
+
+    /**
+     * @brief Flex counter group operations
+     *
+     * @type sai_redis_flex_counter_group_parameter_t
+     * @flags CREATE_AND_SET
+     * @default 0
+     */
+    SAI_REDIS_SWITCH_ATTR_FLEX_COUNTER_GROUP,
+
+    /**
+     * @brief Flex counter operations
+     *
+     * @type sai_redis_counter_parameter_t
+     * @flags CREATE_AND_SET
+     * @default 0
+     */
+    SAI_REDIS_SWITCH_ATTR_FLEX_COUNTER,
+
 } sai_redis_switch_attr_t;
 
 /**

lib/sairediscommon.h
@@ -52,6 +52,11 @@
 #define REDIS_ASIC_STATE_COMMAND_OBJECT_TYPE_GET_AVAILABILITY_QUERY     "object_type_get_availability_query"
 #define REDIS_ASIC_STATE_COMMAND_OBJECT_TYPE_GET_AVAILABILITY_RESPONSE  "object_type_get_availability_response"
 
+#define REDIS_FLEX_COUNTER_COMMAND_START_POLL       "start_poll"
+#define REDIS_FLEX_COUNTER_COMMAND_STOP_POLL        "stop_poll"
+#define REDIS_FLEX_COUNTER_COMMAND_SET_GROUP        "set_counter_group"
+#define REDIS_FLEX_COUNTER_COMMAND_DEL_GROUP        "del_counter_group"
+#define REDIS_FLEX_COUNTER_COMMAND_RESPONSE         "counter_response"
 /**
  * @brief Redis virtual object id counter key name.
  *

syncd/Syncd.cpp
@@ -169,6 +169,8 @@ Syncd::Syncd(
     m_dbFlexCounter = std::make_shared<swss::DBConnector>(m_contextConfig->m_dbFlex, 0);
     m_flexCounter = std::make_shared<swss::ConsumerTable>(m_dbFlexCounter.get(), FLEX_COUNTER_TABLE);
     m_flexCounterGroup = std::make_shared<swss::ConsumerTable>(m_dbFlexCounter.get(), FLEX_COUNTER_GROUP_TABLE);
+    m_flexCounterTable = std::make_shared<swss::Table>(m_dbFlexCounter.get(), FLEX_COUNTER_TABLE);
+    m_flexCounterGroupTable = std::make_shared<swss::Table>(m_dbFlexCounter.get(), FLEX_COUNTER_GROUP_TABLE);
 
     m_switchConfigContainer = std::make_shared<sairedis::SwitchConfigContainer>();
     m_redisVidIndexGenerator = std::make_shared<sairedis::RedisVidIndexGenerator>(m_dbAsic, REDIS_KEY_VIDCOUNTER);
@@ -379,6 +381,18 @@ sai_status_t Syncd::processSingleEvent(
 
     if (op == REDIS_ASIC_STATE_COMMAND_OBJECT_TYPE_GET_AVAILABILITY_QUERY)
         return processObjectTypeGetAvailabilityQuery(kco);
+
+    if (op == REDIS_FLEX_COUNTER_COMMAND_START_POLL)
+        return processFlexCounterEvent(key, SET_COMMAND, kfvFieldsValues(kco));
+
+    if (op == REDIS_FLEX_COUNTER_COMMAND_STOP_POLL)
+        return processFlexCounterEvent(key, DEL_COMMAND, kfvFieldsValues(kco));
+
+    if (op == REDIS_FLEX_COUNTER_COMMAND_SET_GROUP)
+        return processFlexCounterGroupEvent(key, SET_COMMAND, kfvFieldsValues(kco));
+
+    if (op == REDIS_FLEX_COUNTER_COMMAND_DEL_GROUP)
+        return processFlexCounterGroupEvent(key, DEL_COMMAND, kfvFieldsValues(kco));
 
     SWSS_LOG_THROW("event op '%s' is not implemented, FIXME", op.c_str());
 }
@@ -2442,18 +2456,44 @@ void Syncd::processFlexCounterGroupEvent( // TODO must be moved to go via ASIC c
 
     WatchdogScope ws(m_timerWatchdog, op + ":" + groupName, &kco);
 
+    processFlexCounterGroupEvent(groupName, op, values, false);
+}
+
+sai_status_t Syncd::processFlexCounterGroupEvent(
+        _In_ const std::string &groupName,
+        _In_ const std::string &op,
+        _In_ const std::vector<swss::FieldValueTuple> &values,
+        _In_ bool fromAsicChannel)
+{
+    SWSS_LOG_ENTER();
+
     if (op == SET_COMMAND)
     {
         m_manager->addCounterPlugin(groupName, values);
+        if (fromAsicChannel)
+        {
+            m_flexCounterGroupTable->set(groupName, values);
+        }
     }
     else if (op == DEL_COMMAND)
     {
+        if (fromAsicChannel)
+        {
+            m_flexCounterGroupTable->del(groupName);
+        }
         m_manager->removeCounterPlugins(groupName);
     }
     else
     {
         SWSS_LOG_ERROR("unknown command: %s", op.c_str());
     }
+
+    if (fromAsicChannel)
+    {
+        sendApiResponse(SAI_COMMON_API_SET, SAI_STATUS_SUCCESS);
+    }
+
+    return SAI_STATUS_SUCCESS;
 }
 
 void Syncd::processFlexCounterEvent( // TODO must be moved to go via ASIC channel queue
@@ -2469,8 +2509,20 @@ void Syncd::processFlexCounterEvent( // TODO must be moved to go via ASIC channe
 
     auto& key = kfvKey(kco);
     auto& op = kfvOp(kco);
+    auto& values = kfvFieldsValues(kco);
 
     WatchdogScope ws(m_timerWatchdog, op + ":" + key, &kco);
+
+    processFlexCounterEvent(key, op, values, false);
+}
+
+sai_status_t Syncd::processFlexCounterEvent(
+        _In_ const std::string &key,
+        _In_ const std::string &op,
+        _In_ const std::vector<swss::FieldValueTuple> &values,
+        _In_ bool fromAsicChannel)
+{
+    SWSS_LOG_ENTER();
 
     auto delimiter = key.find_first_of(":");
 
@@ -2478,11 +2530,18 @@ void Syncd::processFlexCounterEvent( // TODO must be moved to go via ASIC channe
     {
         SWSS_LOG_ERROR("Failed to parse the key %s", key.c_str());
 
-        return; // if key is invalid there is no need to process this event again
+        if (fromAsicChannel)
+        {
+            sendApiResponse(SAI_COMMON_API_SET, SAI_STATUS_FAILURE);
+        }
+
+        return SAI_STATUS_FAILURE; // if key is invalid there is no need to process this event again
     }
 
     auto groupName = key.substr(0, delimiter);
     auto strVid = key.substr(delimiter + 1);
+
+    auto effective_op = op;
 
     sai_object_id_t vid;
     sai_deserialize_object_id(strVid, vid);
@@ -2491,26 +2550,46 @@ void Syncd::processFlexCounterEvent( // TODO must be moved to go via ASIC channe
 
     if (!m_translator->tryTranslateVidToRid(vid, rid))
     {
-        SWSS_LOG_WARN("port VID %s, was not found (probably port was removed/splitted) and will remove from counters now",
-                sai_serialize_object_id(vid).c_str());
-
-        op = DEL_COMMAND;
+        if (fromAsicChannel)
+        {
+            SWSS_LOG_ERROR("port VID %s, was not found (probably port was removed/splitted) and will remove from counters now",
+                           sai_serialize_object_id(vid).c_str());
+        }
+        else
+        {
+            SWSS_LOG_WARN("port VID %s, was not found (probably port was removed/splitted) and will remove from counters now",
+                          sai_serialize_object_id(vid).c_str());
+        }
+        effective_op = DEL_COMMAND;
     }
 
-    const auto values = kfvFieldsValues(kco);
-
-    if (op == SET_COMMAND)
+    if (effective_op == SET_COMMAND)
     {
         m_manager->addCounter(vid, rid, groupName, values);
+        if (fromAsicChannel)
+        {
+            m_flexCounterTable->set(key, values);
+        }
     }
-    else if (op == DEL_COMMAND)
+    else if (effective_op == DEL_COMMAND)
     {
+        if (fromAsicChannel)
+        {
+            m_flexCounterTable->del(key);
+        }
         m_manager->removeCounter(vid, groupName);
     }
     else
     {
         SWSS_LOG_ERROR("unknown command: %s", op.c_str());
     }
+
+    if (fromAsicChannel)
+    {
+        sendApiResponse(SAI_COMMON_API_SET, SAI_STATUS_SUCCESS);
+    }
+
+    return SAI_STATUS_SUCCESS;
 }
 
 void Syncd::syncUpdateRedisQuadEvent(

syncd/tests/TestSyncdLib.cpp
@@ -8,18 +8,31 @@
 
 #include "TestSyncdLib.h"
 
-void flushAsicDb()
+void flushDb(std::string name)
 {
     SWSS_LOG_ENTER();
 
-    auto db = std::make_shared<swss::DBConnector>("ASIC_DB", 0, true);
+    auto db = std::make_shared<swss::DBConnector>(name, 0, true);
     auto reply = std::make_shared<swss::RedisReply>(db.get(), "FLUSHALL", REDIS_REPLY_STATUS);
 
     reply->checkStatusOK();
 
-    SWSS_LOG_NOTICE("Flushed ASIC DB");
+    SWSS_LOG_NOTICE("Flushed %s", name.c_str());
 }
 
+void flushAsicDb()
+{
+    SWSS_LOG_ENTER();
+
+    flushDb("ASIC_DB");
+}
+
+void flushFlexCounterDb()
+{
+    SWSS_LOG_ENTER();
+
+    flushDb("FLEX_COUNTER_DB");
+}
 void sendSyncdShutdownNotification()
 {
     SWSS_LOG_ENTER();

syncd/tests/TestSyncdLib.h
@@ -1,4 +1,5 @@
 #pragma once
 
 void flushAsicDb();
+void flushFlexCounterDb();
 void sendSyncdShutdownNotification();

commit c8da61f5b9fcf7f80a68e1d6b324c67f6ac35b46

Fix adding flex counter to wrong context
The counters for syncd (switch chip) were attempted to be added to
gbsyncd (gearbox phys), and vice versa.
This issue is introduced by
https://github.com/sonic-net/sonic-sairedis/pull/1362
When setting the redis attribute
SAI_REDIS_SWITCH_ATTR_FLEX_COUNTER_GROUP and
SAI_REDIS_SWITCH_ATTR_FLEX_COUNTER, the operation is applied to every
contexts (both syncd and gbsyncd). However, the counters to initialize
could only exist in one context.

The fix is to check that the target switch id exists in the context; if
not, skip the operation.

lib/RedisRemoteSaiInterface.cpp
@@ -2049,6 +2049,19 @@ sai_switch_notifications_t RedisRemoteSaiInterface::syncProcessNotification(
     return { };
 }
 
+bool RedisRemoteSaiInterface::containsSwitch(
+        _In_ sai_object_id_t switchId) const
+{
+    SWSS_LOG_ENTER();
+
+    if (!m_switchContainer->contains(switchId)){
+        SWSS_LOG_WARN("context %s failed to find switch %s",
+                m_contextConfig->m_name.c_str(), sai_serialize_object_id(switchId).c_str());
+        return false;
+    }
+    return true;
+}
+
 const std::map<sai_object_id_t, swss::TableDump>& RedisRemoteSaiInterface::getTableDump() const
 {
     SWSS_LOG_ENTER();

lib/RedisRemoteSaiInterface.h
@@ -227,6 +227,9 @@ namespace sairedis
 
             const std::map<sai_object_id_t, swss::TableDump>& getTableDump() const;
 
+            bool containsSwitch(
+                    _In_ sai_object_id_t switchId) const;
+
         private: // QUAD API helpers
 
             sai_status_t create(

lib/Sai.cpp
@@ -231,6 +231,15 @@ sai_status_t Sai::set(
 
         for (auto& kvp: m_contextMap)
         {
+            if (objectType == SAI_OBJECT_TYPE_SWITCH && (attr->id == SAI_REDIS_SWITCH_ATTR_FLEX_COUNTER ||
+                    attr->id == SAI_REDIS_SWITCH_ATTR_FLEX_COUNTER_GROUP))
+            {
+                if (!kvp.second->m_redisSai->containsSwitch(objectId))
+                {
+                    continue;
+                }
+            }
+
             sai_status_t status = kvp.second->m_redisSai->set(objectType, objectId, attr);
 
             success &= (status == SAI_STATUS_SUCCESS);