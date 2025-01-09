d6a2726f9254e6dc68f214a5830a87074b47870e
Stephen Sun <stephens@nvidia.com>
2025-01-09 05:59:30 +0000

    Update log severity before and after bulk counter polling to info
    Signed-off-by: Stephen Sun <stephens@nvidia.com>

--- syncd/FlexCounter.cpp
+++ syncd/FlexCounter.cpp
@@ -1034,7 +1034,7 @@ private:
         }
         uint32_t current = 0;
 
-        SWSS_LOG_DEBUG("Before getting bulk %s %s %s size %u bulk chunk size %u current %u", m_instanceId.c_str(), m_name.c_str(), ctx.name.c_str(), size, bulk_chunk_size, current);
+        SWSS_LOG_INFO("Before getting bulk %s %s %s size %u bulk chunk size %u current %u", m_instanceId.c_str(), m_name.c_str(), ctx.name.c_str(), size, bulk_chunk_size, current);
 
         while (current < size)
         {
@@ -1061,6 +1061,8 @@ private:
                 bulk_chunk_size = size - current;
             }
         }
+
+        SWSS_LOG_INFO("After getting bulk %s %s %s total %u objects", m_instanceId.c_str(), m_name.c_str(), ctx.name.c_str(), size);
 
         auto time_stamp = std::chrono::steady_clock::now().time_since_epoch().count();
 
