8ea579a0a988a5020e393158711951fb3d3fe260
Kamil Cudnik <kcudnik@gmail.com>
2025-01-22 17:47:15 +0100

    [syncd] Move logSet logGet under mutex to prevent race condition
    Fixes: https://github.com/sonic-net/sonic-buildimage/issues/21180

--- unittest/syncd/TestVendorSai.cpp
+++ unittest/syncd/TestVendorSai.cpp
@@ -1558,3 +1558,14 @@ TEST_F(VendorSaiTest, bulk_prefix_compression_entry)
     EXPECT_EQ(SAI_STATUS_NOT_SUPPORTED,
             m_vsai->bulkSet(0, e, nullptr, SAI_BULK_OP_ERROR_MODE_STOP_ON_ERROR, nullptr));
 }
+
+TEST(VendorSai, logSet_logGet)
+{
+    VendorSai sai;
+    sai.apiInitialize(0, &test_services);
+
+    EXPECT_EQ(SAI_STATUS_SUCCESS, sai.logSet(SAI_API_PORT, SAI_LOG_LEVEL_DEBUG));
+
+    EXPECT(SAI_LOG_LEVEL_DEBUG, sai.logGet(SAI_API_PORT));
+    EXPECT(SAI_LOG_LEVEL_NOTICE, sai.logGet(SAI_API_SWITCH));
+}
