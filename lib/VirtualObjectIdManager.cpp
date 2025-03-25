sonic-net/sonic-sairedis

commit 8976034d1c314a25e36bcbeba270642458abfa9d

incrementBy returns a list of indices
Signed-off-by: Stepan Blyschak <stepanb@nvidia.com>

lib/VirtualObjectIdManager.cpp
@@ -291,21 +291,20 @@ void VirtualObjectIdManager::allocateNewObjectIds(
 
     uint32_t switchIndex = static_cast<uint32_t>(SAI_REDIS_GET_SWITCH_INDEX(switchId));
 
-    uint64_t lastObjectIndex = m_oidIndexGenerator->incrementBy(count);
-    uint64_t firstObjectIndex = lastObjectIndex - count + 1;
+    std::vector<uint64_t> objectIndexes = m_oidIndexGenerator->incrementBy(count);
 
     const uint64_t indexMax = SAI_REDIS_OBJECT_INDEX_MAX;
 
-    if (lastObjectIndex > indexMax)
+    if (objectIndexes.back() > indexMax)
     {
         SWSS_LOG_THROW("no more object indexes available, given: 0x%" PRIx64 " but limit is 0x%" PRIx64 " ",
-                lastObjectIndex,
+                objectIndexes.back(),
                 indexMax);
     }
 
     for (size_t idx = 0; idx < count; idx++)
     {
-        oids[idx] = constructObjectId(objectTypes[idx], switchIndex, firstObjectIndex + idx, m_globalContext);
+        oids[idx] = constructObjectId(objectTypes[idx], switchIndex, objectIndexes[idx], m_globalContext);
     }
 }
 

lib/OidIndexGenerator.h
@@ -3,6 +3,7 @@
 #include <stdint.h>
 
 #include <saitypes.h> // For SAL annotations
+#include <vector>
 
 namespace sairedis
 {
@@ -17,7 +18,8 @@ namespace sairedis
         public:
 
             virtual uint64_t increment() = 0;
-            virtual uint64_t incrementBy(
+
+            virtual std::vector<uint64_t> incrementBy(
                 _In_ uint64_t count) = 0;
 
             virtual void reset() = 0;

lib/RedisVidIndexGenerator.cpp
@@ -27,7 +27,7 @@ uint64_t RedisVidIndexGenerator::increment()
     return m_dbConnector->incr(m_vidCounterName); // "VIDCOUNTER"
 }
 
-uint64_t RedisVidIndexGenerator::incrementBy(
+std::vector<uint64_t> RedisVidIndexGenerator::incrementBy(
     _In_ uint64_t count)
 {
     SWSS_LOG_ENTER();
@@ -35,7 +35,18 @@ uint64_t RedisVidIndexGenerator::incrementBy(
     swss::RedisCommand sincr;
     sincr.format("INCRBY %s %" PRIu64, m_vidCounterName.c_str(), count);
     swss::RedisReply r(m_dbConnector.get(), sincr, REDIS_REPLY_INTEGER);
-    return r.getContext()->integer;
+    uint64_t lastObjectIndex = r.getContext()->integer;
+    uint64_t firstObjectIndex = lastObjectIndex - count + 1;
+
+    std::vector<uint64_t> result;
+    result.reserve(count);
+
+    for (uint64_t i = firstObjectIndex; i <= lastObjectIndex; ++i)
+    {
+        result.push_back(i);
+    }
+
+    return result;
 }
 
 void RedisVidIndexGenerator::reset()

lib/RedisVidIndexGenerator.h
@@ -23,7 +23,7 @@ namespace sairedis
         public:
 
             virtual uint64_t increment() override;
-            virtual uint64_t incrementBy(
+            virtual std::vector<uint64_t> incrementBy(
                 _In_ uint64_t count) override;
 
             virtual void reset() override;

meta/NumberOidIndexGenerator.cpp
@@ -18,12 +18,20 @@ uint64_t NumberOidIndexGenerator::increment()
     return ++m_index;
 }
 
-uint64_t NumberOidIndexGenerator::incrementBy(
+std::vector<uint64_t> NumberOidIndexGenerator::incrementBy(
     _In_ uint64_t count)
 {
     SWSS_LOG_ENTER();
 
-    return (m_index += count);
+    std::vector<uint64_t> result;
+    result.reserve(count);
+
+    for (uint64_t i = 0; i < count; ++i)
+    {
+        result.push_back(++m_index);
+    }
+
+    return result;
 }
 
 void NumberOidIndexGenerator::reset()

meta/NumberOidIndexGenerator.h
@@ -17,7 +17,7 @@ namespace saimeta
         public:
 
             virtual uint64_t increment() override;
-            virtual uint64_t incrementBy(
+            virtual std::vector<uint64_t> incrementBy(
                 _In_ uint64_t count) override;
 
             virtual void reset() override;

commit 2d3752348b249523bd6422ca0c866ebaf491caaa

fix issue introduced in 8976034d1c314a25e36bcbeba270642458abfa9d
Signed-off-by: Stepan Blyschak <stepanb@nvidia.com>

lib/VirtualObjectIdManager.cpp
@@ -267,6 +267,11 @@ void VirtualObjectIdManager::allocateNewObjectIds(
 {
     SWSS_LOG_ENTER();
 
+    if (count == 0)
+    {
+        return;
+    }
+
     for (size_t idx = 0; idx < count; idx++)
     {
         if (sai_metadata_is_object_type_valid(objectTypes[idx]) == false)