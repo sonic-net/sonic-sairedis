dd9b7e7a929f980d981d5418a337f389752283ce
syuan <syuan@arista.com>
2025-09-01 17:15:55 +0000

    Add POST status notification test.

---
+++ unittest/meta/TestNotificationMacsecPostStatus.cpp
@@ -0,0 +1,61 @@
+#include "NotificationSwitchMacsecPostStatus.h"
+#include "Meta.h"
+#include "MetaTestSaiInterface.h"
+
+#include "sairediscommon.h"
+#include "sai_serialize.h"
+
+#include <gtest/gtest.h>
+
+#include <memory>
+
+using namespace sairedis;
+using namespace saimeta;
+
+static std::string s = "{\"macsec_post_status\":\"pass\",\"macsec_id\":\"oid:0x5800000000\"}";
+static std::string null = "{\"macsec_post_status\":\"pass\",\"macsec_id\":\"oid:0x0\"}";
+
+TEST(NotificationMacsecPostStatus, ctr)
+{
+    NotificationMacsecPostStatus n(s);
+}
+
+TEST(NotificationMacsecPostStatus, getSwitchId)
+{
+    NotificationMacsecPostStatus n(s);
+
+    EXPECT_EQ(n.getSwitchId(), SAI_NULL_OBJECT_ID);
+
+    NotificationMacsecPostStatus n2(null);
+
+    EXPECT_EQ(n2.getSwitchId(), SAI_NULL_OBJECT_ID);
+}
+
+TEST(NotificationMacsecPostStatus, getAnyObjectId)
+{
+    NotificationMacsecPostStatus n(s);
+
+    EXPECT_EQ(n.getAnyObjectId(), 0x5800000000);
+
+    NotificationMacsecPostStatus n2(null);
+
+    EXPECT_EQ(n2.getAnyObjectId(), 0);
+}
+
+static void on_macsec_post_status(
+        _In_ sai_object_id_t switch_id,
+        _In_ sai_macsec_post_status_t post_status)
+{
+    SWSS_LOG_ENTER();
+}
+
+TEST(NotificationMacsecPostStatus, executeCallback)
+{
+    NotificationMacsecPostStatus n(s);
+
+    sai_switch_notifications_t ntfs;
+
+    ntfs.on_macsec_post_status = &on_macsec_post_status;
+
+    n.executeCallback(ntfs);
+}
---
+++ unittest/meta/TestNotificationSwitchMacsecPostStatus.cpp
@@ -0,0 +1,61 @@
+#include "NotificationSwitchMacsecPostStatus.h"
+#include "Meta.h"
+#include "MetaTestSaiInterface.h"
+
+#include "sairediscommon.h"
+#include "sai_serialize.h"
+
+#include <gtest/gtest.h>
+
+#include <memory>
+
+using namespace sairedis;
+using namespace saimeta;
+
+static std::string s = "{\"macsec_post_status\":\"pass\",\"switch_id\":\"oid:0x2100000000\"}";
+static std::string null = "{\"macsec_post_status\":\"pass\",\"switch_id\":\"oid:0x0\"}";
+
+TEST(NotificationSwitchMacsecPostStatus, ctr)
+{
+    NotificationSwitchMacsecPostStatus n(s);
+}
+
+TEST(NotificationSwitchMacsecPostStatus, getSwitchId)
+{
+    NotificationSwitchMacsecPostStatus n(s);
+
+    EXPECT_EQ(n.getSwitchId(), 0x2100000000);
+
+    NotificationSwitchMacsecPostStatus n2(null);
+
+    EXPECT_EQ(n2.getSwitchId(), 0);
+}
+
+TEST(NotificationSwitchMacsecPostStatus, getAnyObjectId)
+{
+    NotificationSwitchMacsecPostStatus n(s);
+
+    EXPECT_EQ(n.getAnyObjectId(), 0x2100000000);
+
+    NotificationSwitchMacsecPostStatus n2(null);
+
+    EXPECT_EQ(n2.getAnyObjectId(), 0);
+}
+
+static void on_switch_macsec_post_status(
+        _In_ sai_object_id_t switch_id,
+        _In_ sai_switch_macsec_post_status_t post_status)
+{
+    SWSS_LOG_ENTER();
+}
+
+TEST(NotificationSwitchMacsecPostStatus, executeCallback)
+{
+    NotificationSwitchMacsecPostStatus n(s);
+
+    sai_switch_notifications_t ntfs;
+
+    ntfs.on_switch_macsec_post_status = &on_switch_macsec_post_status;
+
+    n.executeCallback(ntfs);
+}
