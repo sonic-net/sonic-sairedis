0db780bf9893e33d37da6630918309178b3d4d45
david.zagury <davidza@nvidia.com>
2025-12-16 09:44:34 +0200

    Add UT
    Signed-off-by: david.zagury <davidza@nvidia.com>

--- syncd/Syncd.cpp
+++ syncd/Syncd.cpp
@@ -362,6 +362,35 @@ void Syncd::processEvent(
     while (!consumer.empty());
 }
 
+void Syncd::processEventInShutdownWaitMode(
+        _In_ sairedis::SelectableChannel& consumer)
+{
+    SWSS_LOG_ENTER();
+
+    // Syncd in shutdown-wait mode must respond to INIT_VIEW with FAILURE to avoid deadlock with OA
+    // This could happen because Orchagent sends INIT_VIEW before registering shutdown callback
+    // Can't reorder due to circular dependency: need switch to register callbacks, but
+    //      need INIT_VIEW before creating switch
+
+    swss::KeyOpFieldsValuesTuple kco;
+    consumer.pop(kco, false);
+
+    auto& op = kfvOp(kco);
+    auto& key = kfvKey(kco);
+
+    SWSS_LOG_WARN("Received command while in shutdown-wait mode Command: op=%s, key=%s", op.c_str(), key.c_str());
+
+    if (op == REDIS_ASIC_STATE_COMMAND_NOTIFY)
+    {
+        SWSS_LOG_ERROR("Syncd is waiting for shutdown, cannot process %s Sending FAILURE response", key.c_str());
+        sendNotifyResponse(SAI_STATUS_FAILURE);
+    }
+    else
+    {
+        SWSS_LOG_WARN("Ignoring non-notify command in shutdown-wait mode");
+    }
+}
+
 sai_status_t Syncd::processSingleEvent(
         _In_ const swss::KeyOpFieldsValuesTuple &kco)
 {
@@ -5899,28 +5928,7 @@ void Syncd::run()
             {
                 if (inShutdownWaitMode)
                 {
-                // Syncd in shutdown-wait mode must respond to INIT_VIEW with FAILURE to avoid deadlock with OA
-                // This could happen because Orchagent sends INIT_VIEW before registering shutdown callback
-                // Can't reorder due to circular dependency: need switch to register callbacks, but
-                //      need INIT_VIEW before creating switch
-
-                    swss::KeyOpFieldsValuesTuple kco;
-                    m_selectableChannel->pop(kco, false);
-
-                    auto& op = kfvOp(kco);
-                    auto& key = kfvKey(kco);
-
-                    SWSS_LOG_WARN("Received command while in shutdown-wait mode Command: op=%s, key=%s", op.c_str(), key.c_str());
-
-                    if (op == REDIS_ASIC_STATE_COMMAND_NOTIFY)
-                    {
-                        SWSS_LOG_ERROR("Syncd is waiting for shutdown, cannot process %s Sending FAILURE response", key.c_str());
-                        sendNotifyResponse(SAI_STATUS_FAILURE);
-                    }
-                    else
-                    {
-                        SWSS_LOG_WARN("Ignoring non-notify command in shutdown-wait mode");
-                    }
+                    processEventInShutdownWaitMode(*m_selectableChannel.get());
                 }
                 else
                 {
--- unittest/syncd/TestSyncd.cpp
+++ unittest/syncd/TestSyncd.cpp
@@ -678,4 +678,40 @@ TEST_F(SyncdTest, BulkRemoveTest)
 
     m_syncd->processEvent(*channel);
 }
+
+TEST_F(SyncdTest, processEventInShutdownWaitMode_NotifyCommand)
+{
+    // Test that NOTIFY commands receive FAILURE response in shutdown-wait mode
+    auto channel = std::make_shared<MockSelectableChannel>();
+
+    EXPECT_CALL(*channel, pop(testing::_, testing::_))
+        .WillOnce(testing::Invoke([](swss::KeyOpFieldsValuesTuple& kco, bool initViewMode) {
+            kfvKey(kco) = SYNCD_INIT_VIEW;
+            kfvOp(kco) = REDIS_ASIC_STATE_COMMAND_NOTIFY;
+        }));
+
+    // Verify that set() is called with FAILURE status response
+    EXPECT_CALL(*channel, set(testing::_, testing::_, REDIS_ASIC_STATE_COMMAND_NOTIFY))
+        .Times(1);
+
+    m_syncd->processEventInShutdownWaitMode(*channel);
+}
+
+TEST_F(SyncdTest, processEventInShutdownWaitMode_NonNotifyCommand)
+{
+    // Test that non-NOTIFY commands are ignored (no response sent) in shutdown-wait mode
+    auto channel = std::make_shared<MockSelectableChannel>();
+
+    EXPECT_CALL(*channel, pop(testing::_, testing::_))
+        .WillOnce(testing::Invoke([](swss::KeyOpFieldsValuesTuple& kco, bool initViewMode) {
+            kfvKey(kco) = "SAI_OBJECT_TYPE_SWITCH:oid:0x21000000000000";
+            kfvOp(kco) = REDIS_ASIC_STATE_COMMAND_CREATE;
+        }));
+
+    // Verify that set() is NOT called for non-notify commands
+    EXPECT_CALL(*channel, set(testing::_, testing::_, testing::_))
+        .Times(0);
+
+    m_syncd->processEventInShutdownWaitMode(*channel);
+}
 #endif
