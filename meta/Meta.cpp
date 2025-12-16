4fdec1cb2eab69746b4b51e97e8a59353b216eb7
Yogapriya Mohankumar <ymohanku@cisco.com>
2025-12-16 03:46:26 -0800

    fix for ERR swss#orchagent: :- meta_sai_validate_fdb_entry: object key SAI_OBJECT_TYPE_FDB_ENTRY
    Signed-off-by: Yogapriya Mohankumar <ymohanku@cisco.com>

--- meta/Meta.cpp
+++ meta/Meta.cpp
@@ -2137,7 +2137,7 @@ sai_status_t Meta::meta_sai_validate_fdb_entry(
 
     if (!m_saiObjectCollection.objectExists(meta_key_fdb) && !get)
     {
-        SWSS_LOG_ERROR("object key %s doesn't exist",
+        SWSS_LOG_WARN("object key %s doesn't exist",
                 sai_serialize_object_meta_key(meta_key_fdb).c_str());
 
         return SAI_STATUS_ITEM_NOT_FOUND;
