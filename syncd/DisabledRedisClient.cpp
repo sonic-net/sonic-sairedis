df16a2be11bd4bf6f7dba375595fa2ecf4771847
Connor Roos <croos@nvidia.com>
2026-01-13 22:22:01 +0000

    Fix whitespace
    Signed-off-by: Connor Roos <croos@nvidia.com>

--- syncd/DisabledRedisClient.cpp
+++ syncd/DisabledRedisClient.cpp
@@ -273,7 +273,7 @@ void DisabledRedisClient::getVidsForRids(
         _Out_ sai_object_id_t* vids)
 {
     SWSS_LOG_ENTER();
-    
+
     for (size_t i = 0; i < count; i++)
     {
         vids[i] = SAI_NULL_OBJECT_ID;
