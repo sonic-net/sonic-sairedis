7190c764f3806d01b1a50497cb86bd3beeb7d7f0
Justin Wong <jvwong@arista.com>
2026-02-26 03:34:55 +0000

    Fix more typos
    Signed-off-by: Justin Wong <jvwong@arista.com>

--- syncd/FlexCounter.cpp
+++ syncd/FlexCounter.cpp
@@ -1622,7 +1622,7 @@ private:
         }
         else
         {
-            // Query succeeded, intersect with supplied set to no query uninteneded counters
+            // Query succeeded, intersect with supplied set to no query unintended counters
             std::set<StatType> originalSet(counter_ids.begin(), counter_ids.end());
             std::set<StatType> intersected;
             std::set_intersection(counter_ids_set.begin(), counter_ids_set.end(),
@@ -3914,7 +3914,7 @@ void FlexCounter::addCounter(
 
             }
             catch (...) {
-                SWSS_LOG_WARN("Error occured initlizating SAI objects with counter groups, falling back to global counter list implementation");
+                SWSS_LOG_WARN("Error occurred initializing SAI objects with counter groups, falling back to global counter list implementation");
                 getCounterContext(counterGroupRef->second)->addObject(
                         vid,
                         rid,
@@ -3987,7 +3987,7 @@ void FlexCounter::bulkAddCounter(
             }
             catch (...)
             {
-                SWSS_LOG_WARN("Error occured initlizating SAI objects with counter groups, falling back to global counter list implementation");
+                SWSS_LOG_WARN("Error occurred initializing SAI objects with counter groups, falling back to global counter list implementation");
                 getCounterContext(counterGroupRef->second)->bulkAddObject(
                         vids,
                         rids,
@@ -4027,7 +4027,7 @@ void FlexCounter::bulkAddCounter(
         }
         catch (...)
         {
-            SWSS_LOG_WARN("Error occured initlizating SAI objects with counter groups, falling back to global counter list implementation");
+            SWSS_LOG_WARN("Error occurred initializing SAI objects with counter groups, falling back to global counter list implementation");
             getCounterContext(COUNTER_TYPE_BUFFER_POOL)->bulkAddObject(
                     vids,
                     rids,
