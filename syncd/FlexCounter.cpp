332d0ed4eb98793c673edf9dcc21c41f97e1fe65
Justin Wong <jvwong@arista.com>
2026-02-26 03:39:22 +0000

    Fix comment wording
    Signed-off-by: Justin Wong <jvwong@arista.com>

--- syncd/FlexCounter.cpp
+++ syncd/FlexCounter.cpp
@@ -1622,7 +1622,7 @@ private:
         }
         else
         {
-            // Query succeeded, intersect with supplied set to no query unintended counters
+            // Query succeeded, intersect with supplied set to not query unintended counters
             std::set<StatType> originalSet(counter_ids.begin(), counter_ids.end());
             std::set<StatType> intersected;
             std::set_intersection(counter_ids_set.begin(), counter_ids_set.end(),
