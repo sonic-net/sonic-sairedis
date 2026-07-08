a01722c15bc7cfc082ade2a67343f7b5f42fbf5d
Nicholas Ching <nicholaslching@gmail.com>
2026-07-08 13:49:46 -0700

    fix: satisfy aspell and SWSS_LOG_ENTER checks in UT harness preload shim
    Signed-off-by: Nicholas Ching <nicholaslching@gmail.com>

--- .azure-pipelines/docker-sai-test-vpp/swss_log_stdout_preload.cpp
+++ .azure-pipelines/docker-sai-test-vpp/swss_log_stdout_preload.cpp
@@ -1,6 +1,6 @@
-// Harness-only LD_PRELOAD shim: route libsaivs SWSS_LOG_* to stdout so
-// run_test.sh capture in /var/log/saiserver.log works without pulling SONiC
-// logger setup back into saiserver.cpp (see SAI saithriftv2 saiserver change).
+// Harness-only LD_PRELOAD shim: route SAI VS library SWSS_LOG_* to stdout so
+// run_test.sh capture in /var/log/sai-server.log works without pulling SONiC
+// logger setup back into the SAI server binary.
 #include <swss/logger.h>
 
 namespace {
@@ -8,6 +8,7 @@ namespace {
 __attribute__((constructor))
 static void route_swss_log_to_stdout()
 {
+    SWSS_LOG_ENTER();
     swss::Logger::setMinPrio(swss::Logger::SWSS_DEBUG);
     swss::Logger::swssOutputNotify("saiserver", "STDOUT");
 }
